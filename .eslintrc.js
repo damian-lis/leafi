dc99d887830216b8383818ac3d858ea37dfbb6c6
Damian Lis <foxprogramming93@gmail.com>
2021-06-18 20:09:06 +0200

    Corrected some files based on the prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,27 @@ module.exports = {
     ecmaVersion: 2020, // Use the latest ecmascript standard
     sourceType: 'module', // Allows using import/export statements
     ecmaFeatures: {
-      jsx: true, // Enable JSX since we're using React
-    },
+      jsx: true // Enable JSX since we're using React
+    }
   },
   settings: {
     react: {
-      version: 'detect', // Automatically detect the react version
-    },
+      version: 'detect' // Automatically detect the react version
+    }
   },
   env: {
     browser: true, // Enables browser globals like window and document
-    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    amd: true // Enables require() and define() as global variables as per the amd spec.
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+    'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
     'react/prop-types': 'off',
     'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-    'react/react-in-jsx-scope': 'off',
-  },
-}
+    'react/react-in-jsx-scope': 'off'
+  }
+};
