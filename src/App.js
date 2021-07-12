a0f6c4d100e694262c8f19e193abcaef7057e779
Damian Lis <foxprogramming93@gmail.com>
2021-07-12 22:07:58 +0200

    Added created elements to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import './default.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from 'components/Header';
+import Homepage from 'pages/Homepage';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="main">
+          {/* <Switch> */}
+          <Route path="/">
+            <Homepage />
+          </Route>
+          {/* </Switch> */}
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
